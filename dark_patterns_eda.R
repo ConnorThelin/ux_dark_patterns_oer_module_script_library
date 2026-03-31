8acd3ae33540824116033ba70489d9dc14e3d0c2
ConnorThelin <Connor_Thelin@hotmail.com>
2026-03-31 13:52:51 -0700

    Clean column names and simplify

--- dark_patterns_eda.R
+++ dark_patterns_eda.R
@@ -18,9 +18,19 @@ load_or_install <- function(pkg) {
 packages <- c("tidyverse",
               "ggplot2",
               "googlesheets4",
-              "googledrive"
+              "googledrive",
+              "digest",
+              "janitor"
               )
 invisible(lapply(packages, load_or_install))
+
+# Usages:
+# tidyverse: Base
+# ggplot2: Visualizations
+# googlesheets4: Google Sheet integration
+# googledrive: Google Drive Integration
+# digest: anonymization
+# janitor: clean_names()
 
 
 # RAW DATA ----------------------------------------------------------------
@@ -29,3 +39,22 @@ if (!exists("raw_data")) {
   sheet_url <- "https://docs.google.com/spreadsheets/d/1vMhttsyyit3jwvAmZ0DEi_jWj7Hp-RS5SQR6I_E1ZFQ/edit?usp=sharing"
   raw_data <- read_sheet(sheet_url)
 }
+
+
+# RENAME ROWS -------------------------------------------------------------
+
+modified_data <- raw_data %>%
+  clean_names() %>%
+  rename(
+    name = "your_name_as_it_appears_in_canvas_will_be_anonymized_before_incidents_are_shared_with_the_class",
+    dark_pattern_interaction = "did_you_have_any_interactions_today_that_you_suspect_might_involve_a_ux_dark_pattern",
+    link_to_system = "if_you_reported_yes_or_unsure_please_answer_the_following_questions_name_and_link_to_the_system_or_service",
+    task = "what_were_you_trying_to_do",
+    suspisious_element = "what_was_suss",
+    personal_impact = "did_encountering_this_pattern_impact_you_in_any_way_please_explain_examples_of_possible_impacts_include_but_are_not_limited_to_not_achieving_what_you_want_to_accomplish_hindering_what_you_wanted_to_accomplish_costing_you_more_time_money_or_effort_than_anticipated_desired_less_confidence_or_less_certainty_that_you_have_achieved_your_aim_less_confidence_or_less_certainty_that_you_have_mitigated_undesirable_consequences_undue_emotional_labor_possible_consequences_for_others_and_so_on",
+    concern_level = "based_on_your_personal_feelings_at_this_time_please_mark_the_degree_of_concern_you_have_about_this_incident_no_concern_1_mild_concern_2_moderate_concern_3_high_concern_4_extreme_concern_5",
+    image_captured = "images_or_video_of_a_problematic_interaction_are_very_helpful_for_analysis_did_you_capture_any_images_or_video_of_the_interaction_you_are_reporting_on_we_will_reach_out_when_the_collection_period_is_concluded_to_collect",
+    involved_pattern = "which_ux_dark_patterns_do_you_think_might_be_involved_if_any_check_all_that_apply_patterns_are_from_appendix_a_of_2022_us_ftc_staff_report_bringing_dark_patterns_to_light",
+    comments = "anything_else",
+    image = "if_you_have_screenshots_or_other_documentation_you_can_upload_to_10_files_of_100_mb_in_size_via_one_submission"
+  )
